a4d4c1be258ac54e38ccdd8da4997935027f317c
Karris  Black <karris.b@gmail.com>
2020-12-30 17:20:12 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,57 @@
+
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  cars = ["Gle", "M5", "E63", "RS5"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  more_life = ["time", "long"]
+  more_life.push("arrays!")
   
 end
-
 def add_element_to_start_of_array(array, element)
-  
+    life_live = ["earth", "world"]
+    life_live.unshift("wow")
+    
 end
 
 def remove_element_from_end_of_array(array)
+  time_keep = ["earth", "world", "arrays!"]
+  time_keep.pop
   
 end
 
+
 def remove_element_from_start_of_array(array)
-  
+    left_right = ["wow", "down", "up"]
+    left_right.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
+    count_up = ["pm", "am", "east"]
+    count_up[1]
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+    count_down = ["wow", "right", "round"]  
+    count_down[0]
+
 end
 
 def retrieve_last_element_from_array(array)
+    tom_ford = ["move", "stand", "arrays!"]
+    tom_ford[-1]
   
 end
 
 def update_element_from_index(array, index_number, element)
+    corona_cold = ["run", "duck", "climb"]
+    corona_cold[1] = "totally"
+    
 
 end
